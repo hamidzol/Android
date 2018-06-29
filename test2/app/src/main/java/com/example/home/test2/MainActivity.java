d450cca429048fa127d4038afee55be210b71381
hamid <zolfaghari_znu@yahoo.com>
2018-06-29 23:41:23 +0430

    970409

--- test2/app/src/main/java/com/example/home/test2/MainActivity.java
+++ test2/app/src/main/java/com/example/home/test2/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button bb;
+        bb=(Button)findViewById(R.id.ssss);
+        bb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e( "Tag2","44444You Click cancel!!!!!");
+                Toast.makeText(getApplicationContext(),"jjjjjjjjjjjjjj",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void do_my_work(View view){
@@ -18,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         Log.e( "ttttt","ddddddddddd");
 
     }
-    public void do_cancel(View view){
+    public void do_cancel(){
 
         int i =10;
         Log.e( "Tag2","You Click cancel!!!!!");
